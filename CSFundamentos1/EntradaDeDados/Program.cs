182e9eb9bde622f731b91617cb40dc7d81998ca7
alife-echo <alifesilva90@gmail.com>
2023-12-28 22:32:40 -0300

    input

--- CSFundamentos1/EntradaDeDados/Program.cs
+++ CSFundamentos1/EntradaDeDados/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("\n ## Entrada de dados");
+
+//ReadLine(): le a linha digitada e retorna o que foi digitado
+//Read(): le o que foi digitado(um unico caractere) e retorna o valor em ASCII correspodente
+//ReadKey():le apenas um caractere, usando para segurar a tela até que o usuário pressione uma tecla
+
+Console.WriteLine("\n Informe o seu nome");
+
+string nome = Console.ReadLine() ?? "";
+
+Console.WriteLine($"\nO seu nome é {nome}");
+
+Console.WriteLine($"\n Informe sua idade {nome}");
+int idade = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"\n A idade de {nome} é {idade}");
+Console.ReadKey();
