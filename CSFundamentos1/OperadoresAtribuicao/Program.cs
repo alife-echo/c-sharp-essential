304abce981e4cade073d0c6d4a032cee8c81b1ba
alife-echo <alifesilva90@gmail.com>
2024-01-01 20:20:56 -0300

    operadores atribuicao

--- CSFundamentos1/OperadoresAtribuicao/Program.cs
+++ CSFundamentos1/OperadoresAtribuicao/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Operadores de atribuição\n");
+
+// usando operadores atribuição com tipos numéricos
+
+var x = 10;
+var y = "123";
+y += "456";
+Console.WriteLine($"Valor inicial de x={x}");
+Console.WriteLine($"x+=5 ==> {x+=5}");
+Console.WriteLine($"x-=5 ==> {x-=3}");
+Console.WriteLine($"x*=4 ==> {x*=4}");
+Console.WriteLine($"x/=5 ==> {x/=5}");
+Console.WriteLine($"x%=5 ==> {x%=5}");
+Console.WriteLine(y);
+
