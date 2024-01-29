4fd65b8cdf9a5851afaa544f640ebae1b468a870
alife-echo <alifesilva90@gmail.com>
2024-01-29 08:47:20 -0300

    chore: add anotations for study

--- InstrucaoIF/InstrucaoFor/Program.cs
+++ InstrucaoIF/InstrucaoFor/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Instrução for");
+/*
+for(int i = 1; i <= 10; i++)
+{
+    for(int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i}X{j}={i*j}");
+  
+    }
+    Console.WriteLine("\n");
+}*/
+/*
+for(int i = 0, j = 0; i + j < 10; i++, j++)
+{
+    Console.WriteLine($"i = {i} e j {j}");
+}
+*/
+
+int i = 1;
+for (; ;)
+{
+    Console.WriteLine($"Loop for: Iteração {i}");
+    i++;
+}
