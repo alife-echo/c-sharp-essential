ebee4e751fc3c5b654e9a9c41023397a1f77240c
alife-echo <alifesilva90@gmail.com>
2024-01-31 17:41:49 -0300

    chore: add questions  2 to 5 exercise

--- InstrucaoIF/ExercicioEstrutrurasDeControle/Program.cs
+++ InstrucaoIF/ExercicioEstrutrurasDeControle/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Exercicio Estruturas de controle");
-
+/*
 int x1 = 0, y1 = 0, z1 = 0;
 
 Console.WriteLine("Digite o primeiro numero");
@@ -30,4 +30,115 @@ switch (highestValue)
         Console.WriteLine("Error ao encontrar o maior valor");
         break;
 }
+*/
+/*
+int   a,b,c;
+double x1, x2, delta;
+Console.WriteLine("Informe o valor de a:");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Informe o valor de b:");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Informe o valor de c");
+c = Convert.ToInt32(Console.ReadLine());
+delta = Math.Pow(b, 2) - 4 * a * c;
+x1 = (-b + Math.Sqrt(delta))/(2 * a) ;
+x2 = (-b - Math.Sqrt(delta)) / (2 * a);
 
+if(delta > 0)
+{
+    Console.WriteLine("As raizes são reais e diferentes");
+    Console.WriteLine($"Primeira raiz x1={x1}");
+    Console.WriteLine($"Segunda raiz x2={x2}");
+}
+else if (delta == 0)
+{
+    Console.WriteLine("As raizes são reais e iguais");
+    Console.WriteLine($"Primeira raiz x1={x1}");
+    Console.WriteLine($"Segunda raiz x2={x2}");
+}
+else
+{
+    Console.WriteLine("As raizes são complexas ou imaginarias");
+}
+*/
+
+int swap = 0;
+/*
+Console.WriteLine("Os 10 primeiros números naturais são:");
+for (int i = 1; i <= 10; i++)
+{
+    Console.Write($" {i}");
+    swap += i;
+}
+Console.WriteLine("\n");
+Console.WriteLine($"A soma dos números é:{swap}");*/
+
+
+/*
+int i = 1;
+Console.WriteLine("Os 10 primeiros números naturais são:");
+do
+{
+    Console.Write($" {i}");
+    swap += i;
+    i++;
+} while (i <= 10);
+Console.WriteLine("\n");
+Console.WriteLine($"A soma dos números é:{swap}");
+*/
+/*
+Console.WriteLine("Os 10 primeiros números naturais são:");
+while (i <= 10)
+{
+    Console.Write($" {i}");
+    swap += i;
+    i++;
+}
+Console.WriteLine("\n");
+Console.WriteLine($"A soma dos números é : {swap}");
+*/
+/*
+int x;
+while (true){
+    Console.WriteLine("Digite um número (-1 sair)");
+    x = Convert.ToInt32(Console.ReadLine()); 
+    if( x > 0)
+    {
+        for(int i = 1; i <= 10; i++) {
+            Console.WriteLine($"{i}X{x}={i*x}");
+        }
+    }
+    if (x == -1)
+    {
+        break;
+    }
+    if ( x < 0 || x == 0 )
+    {
+        Console.WriteLine("Informe um valor corretamente");
+    }
+    
+}
+*/
+/*
+bool x = true;
+string? correctValue;
+while (x)
+{
+    Console.WriteLine("Qual a instrução para sair de um loop ?");
+    Console.WriteLine("a.quit");
+    Console.WriteLine("b.continue");
+    Console.WriteLine("c.break");
+    Console.WriteLine("d.exit");
+    Console.WriteLine("Qual a opcção correta ? (Tecle x para sair)");
+    correctValue = Console.ReadLine();
+    if (correctValue == "x" || correctValue == "X")
+    {
+        x = false;
+    }
+    else if (correctValue != "c") {
+        Console.WriteLine("Resposta errada");
+    }
+    if(correctValue == "c") {
+        Console.WriteLine("Resposta correta");
+    }
+}*/
