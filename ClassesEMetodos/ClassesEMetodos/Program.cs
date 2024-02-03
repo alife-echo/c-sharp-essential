6c8aed4c9e5db3563c35b8268d6f31039225dcbb
alife-echo <alifesilva90@gmail.com>
2024-02-03 01:07:10 -0300

    chore: add anotations class

---
+++ ClassesEMetodos/ClassesEMetodos/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Classes E Metodos");
+Console.ReadKey();
+
+
+Pessoa p1 = new();
+p1.nome = "Maria";
+p1.idade = 25;
+p1.sexo = "feminino";
+Console.WriteLine($"{p1.nome} {p1.idade} {p1.sexo}");
+Pessoa p2 = new()
+{
+    nome = "Alife",
+    idade = 23,
+    sexo = "masculino"
+};
+Console.WriteLine($"{p2.nome} {p2.idade} {p2.sexo}");
+class Pessoa
+{
+    public string? nome;
+    public int idade;
+    public string? sexo;
+}
