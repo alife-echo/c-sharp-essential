6d456c070547fe63e3bd7c61b33a61e1b5e519b6
alife-echo <alifesilva90@gmail.com>
2024-02-06 21:16:42 -0300

    construtorII

--- ClassesEMetodos/ConstrutorII/Program.cs
+++ ClassesEMetodos/ConstrutorII/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Criando mais de um construtor");
+
+Aluno aluno = new Aluno("Alife",22,"feminino","A");
+
+Console.WriteLine(aluno?.Nome == null ? "null" : aluno?.Nome);
+Console.WriteLine(aluno?.Idade == 0 ? 0 : aluno?.Idade);
+Console.WriteLine(aluno?.Sexo == null ? "null" : aluno.Sexo);
+Console.WriteLine(aluno?.Aprovado == null ? "null" : aluno.Aprovado);
+public class Aluno
+{   
+    public Aluno(string nome)=>Nome = nome;    
+
+    public Aluno(string nome ,int idade, string sexo, string aprovado):this(nome)
+    {
+        Idade = idade;
+        Sexo = sexo;    
+        Aprovado = aprovado;
+    }
+    public string Nome;
+    public int Idade;
+    public string Sexo;
+    public string Aprovado;
+}
