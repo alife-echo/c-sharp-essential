5301446ae62a3e04c7aa6727fa3fc933ca24adbe
alife-echo <alifesilva90@gmail.com>
2024-02-13 20:58:12 -0300

    metodos com retorno

---
+++ ClassesEMetodos/ExercicioPratico2/Program.cs
@@ -0,0 +1,41 @@
+﻿
+Console.WriteLine("Exercicio");
+
+Carro _ = new("Sedan", "Chevrolet");
+Carro __ = new("SUV", "Ford");
+Console.WriteLine(_.VelocidadeMaxima(300));
+class Carro
+{
+    public string? Modelo;
+    public string? Montadora;
+    public string? Marca;
+    public int Ano;
+    public int Potencia;
+    public Carro(string modelo, string montadora)
+    {
+        this.Modelo = modelo;
+        this.Montadora = montadora;
+        this.Marca = "X";
+        this.Ano = 0000;
+        this.Potencia = 0000;
+        this.Exibir();
+        this.Acelerar(this);
+    }
+    public void Exibir()
+    {
+        Console.WriteLine($"{Modelo},{Montadora},{Marca},{Ano},{Potencia}");
+    }
+    public void Acelerar(Carro c)
+    {
+        Console.WriteLine($"Acelerando o meu {c.Marca}");
+    }
+    public double VelocidadeMaxima (int potencia)
+    {
+        return potencia * 1.75;            
+    }
+}
+
+
+
+//h -> os valores posteriores do fornecido da questão serão estaticos
+//j -> ira retorna um valor de um metodo ao seu chamador
---
+++ ClassesEMetodos/MetodosRetorno/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Calculadora calc = new();
+
+Console.WriteLine($"{calc.Somar(10,10)}");
+Console.WriteLine($"{calc.Subtrair(20,10)}");
+Console.WriteLine($"{calc.Multiplicar(2,10)}");
+Console.WriteLine($"{calc.Dividir(50,10)}");
+
+public class Calculadora
+{
+    public int Somar(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+    public int Subtrair(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+    public int Multiplicar(int n1, int n2)
+    {
+        return n1 * n2;
+    }
+    public int Dividir(int n1, int n2)
+    {
+        return n1 / n2;
+    }
+}
