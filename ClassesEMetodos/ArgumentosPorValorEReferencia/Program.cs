d3db08631673d52dd62e8b05da483e2ff2ba59a8
alife-echo <alifesilva90@gmail.com>
2024-02-14 18:37:42 -0300

    passagem de valor e referencia

---
+++ ClassesEMetodos/ArgumentosPorValorEReferencia/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Passando Argumentos por Valor");
+
+int x = 20;
+Console.WriteLine("\n Valor do argumento x antes de passar por valor : " + x);
+Console.WriteLine("\n--------------------------------------");
+
+Calculo calc = new();
+calc.Dobrar(x);
+
+Console.WriteLine("\n Valor do argumento x depois de passar por valor : " + x);
+Console.WriteLine("\n--------------------------------------");
+public class Calculo
+{
+    public void Dobrar(int y)
+    {
+        y *= 2;
+        Console.WriteLine("\nValor do parâmetro y no método Dobrar " + y);
+    }
+}
---
+++ ClassesEMetodos/PassagemDeArgumentosPoReferenciaRef/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Passagem de argumentos por referencia (ref)");
+
+
+int x = 20;
+Console.WriteLine("\n Valor do argumento x antes de passar por valor : " + x);
+Console.WriteLine("\n--------------------------------------");
+
+Calculo calc = new();
+calc.Dobrar(ref x); // passagem da cópia da referência da memória do argumento x para o parâmetro y
+
+Console.WriteLine("\n Valor do argumento x depois de passar por valor : " + x);
+Console.WriteLine("\n--------------------------------------");
+public class Calculo
+{
+    public void Dobrar(ref int y)
+    {
+        y *= 2;
+        Console.WriteLine("\nValor do parâmetro y no método Dobrar " + y);
+    }
+}
