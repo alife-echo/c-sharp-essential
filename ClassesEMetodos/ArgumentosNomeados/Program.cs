9a376a0024945e0733af05af67c29f3ad4879978
alife-echo <alifesilva90@gmail.com>
2024-02-21 09:11:16 -0300

    parametros nomeados

---
+++ ClassesEMetodos/ArgumentosNomeados/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Argumentos nomeados");
+
+
+Email e1 = new Email();
+
+e1.Enviar(assunto: "Reunião Orçamento",destino:"teste@gmail.com",titulo:"Urgente");
+
+public class Email
+{
+    public void Enviar(string destino,string titulo,string assunto)
+    {
+        Console.WriteLine($"\nPara {destino} - {titulo} \n Assunto: {assunto}");
+    }
+}
---
+++ ClassesEMetodos/ExercicioPratico4/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Exercicio Pratico 4");
+Console.WriteLine("Digite a potencia do carro");
+int p = Convert.ToInt32(Console.ReadLine());
+Carro c1 = new Carro();
+int vc = c1.AumentarPotenciaVelocidade(p,out double velocidade);
+
+Console.WriteLine($"Nova Potencia:{vc},Nova Velocidade:{velocidade}");
+
+public class Carro
+{
+    public int AumentarPotenciaVelocidade (int potenciaVelocidade, out double velocidade)
+    {
+        potenciaVelocidade += 7;
+        velocidade = potenciaVelocidade * 1.75;
+        return potenciaVelocidade;
+    }  
+}
