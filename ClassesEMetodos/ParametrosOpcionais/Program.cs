f8321b923ee4995ec3b808714a84a6f3ce2b93c7
alife-echo <alifesilva90@gmail.com>
2024-02-21 09:48:56 -0300

    parametros opcionais

--- ClassesEMetodos/ParametrosOpcionais/Program.cs
+++ ClassesEMetodos/ParametrosOpcionais/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Parâmetros opcionais");
+
+Console.WriteLine("Informe o destino");
+string? destino = Console.ReadLine();
+
+Console.WriteLine("Informe o titulo");
+string titulo = Console.ReadLine();
+
+Console.WriteLine("Informe o assunto");
+string assunto  = Console.ReadLine();   
+
+
+Email e1 = new Email();
+
+e1.Enviar(destino);
+e1.Enviar(destino, titulo);
+e1.Enviar(destino,titulo,assunto);
+// não posso fazer
+//email.Enviar(destino,,assunto); // causa um error
+e1.Enviar(destino, assunto: assunto); // alternativa
+
+
+Console.ReadKey();
+public class Email
+{
+    public void Enviar(string destino, string titulo = "Titulo Padrão", string assunto = "Assunto Padrão")
+    {
+        Console.WriteLine($"{destino},{titulo} : {assunto}");
+    }
+}
