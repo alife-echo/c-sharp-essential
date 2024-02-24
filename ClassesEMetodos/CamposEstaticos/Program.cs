13aca99efdd345f0ade5939a2e154dfef224ba72
alife-echo <alifesilva90@gmail.com>
2024-02-23 23:18:30 -0300

    campos estaticos

---
+++ ClassesEMetodos/CamposEstaticos/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Membros Estáticos");
+
+A a1 = new A();
+a1.x = 10;
+
+A a2 = new A();
+a2.x = 20;
+
+public class A
+{
+    public int x;
+    public static int y;
+}
