97eaed1547a53d1038d322066e82267564178f6c
alife-echo <alifesilva90@gmail.com>
2024-02-26 23:15:18 -0300

    exercicio pratico 6

---
+++ ClassesEMetodos/ExercicioPratico6/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Exercício Prático 6");
+
+Console.ReadKey();
+
+Console.WriteLine($"IPVA:{Carro.ObterValorIPVA()}");
+class Carro
+{
+    public static double ValorIpva;
+
+    public static double ObterValorIPVA()
+    {
+        ValorIpva = 4;
+        return ValorIpva;
+        
+    }
+}
