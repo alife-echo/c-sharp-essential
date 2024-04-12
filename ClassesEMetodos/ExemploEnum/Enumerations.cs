7968cd3caeefd6c8a651acf33d3f77fda8bef882
alife-echo <alifesilva90@gmail.com>
2024-04-11 22:49:48 -0300

    chore: add example enum

---
+++ ClassesEMetodos/ExemploEnum/Enumerations.cs
@@ -0,0 +1,21 @@
+﻿namespace _18Enumeration;
+
+        enum Categorias
+        {
+            Moda,
+            Automotivo,
+            Artes,
+            Bebidas,
+            Brinquedos
+        }
+        enum DiasDaSemana
+        {
+            Segunda,
+            Terça,
+            Quarta,
+            Quinta,
+            Sexta,
+            Sabado,
+            Domingo
+        }
+    
---
+++ ClassesEMetodos/ExemploEnum/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using _18Enumeration;
+
+
+Console.WriteLine("## Enum ## \n");
+
+foreach (var category in Enum.GetValues(typeof(Categorias))){
+    Console.WriteLine($"Nome:{category},Valor:{(int)category}");
+}
+Console.WriteLine("\nSelecione a Categoria teclando o seu valor");
+int valor = Convert.ToInt32(Console.ReadLine());
+
+var nomeMembroEnum = (Categorias)valor;
+Console.WriteLine($"\n Voce Selecionou a categoria : {nomeMembroEnum.ToString()}");
+
+Console.WriteLine(DiasDaSemana.Domingo);
+Console.WriteLine(DiasDaSemana.Terça);
+
+int dia1 = (int)DiasDaSemana.Domingo;
+int dia2 = (int)DiasDaSemana.Terça;
+
+Console.WriteLine($"\n {DiasDaSemana.Domingo} vale : {dia1}");
+Console.WriteLine($"\n {DiasDaSemana.Terça} vale : {dia2}");
+Console.ReadKey();
