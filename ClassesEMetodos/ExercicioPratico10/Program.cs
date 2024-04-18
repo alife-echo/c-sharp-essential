0ae1a13bd488255f1562fa62811f697b98489eb6
alife-echo <alifesilva90@gmail.com>
2024-04-18 13:50:17 -0300

    chore: finally exercise 10

---
+++ ClassesEMetodos/ExercicioPratico10/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Exercício Prático 10");
+
+Car c1 = new Car(2.3);
+Car.ShowIpva();
+
+Car c2 = new Car(3);
+Car.ShowIpva(); 
+
+class Car
+{
+    private static double ValorIpva;
+
+    public Car(double ipva) 
+    {
+        ValorIpva = ipva;
+    }    
+
+    public static  void ShowIpva()
+    {
+        Console.WriteLine($"Valor Ipva${ValorIpva}");
+    }
+}
