0f922093edbeb06bddb0c0103e94f94b69a7c028
alife-echo <alifesilva90@gmail.com>
2024-04-18 20:10:42 -0300

    chore: add anonymus types

---
+++ ClassesEMetodos/TiposAnonimos/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tipos Anonimos");
+
+
+var aluno = new
+{
+    Nome = "Maria",
+    Idade = 42
+};
+
+Console.WriteLine(aluno.GetType().ToString());
+
+Console.WriteLine($"{aluno.Nome} {aluno.Idade}");
+
+
+var aluno2 = new
+{
+    Id = 1,
+    Nome = "Jairo",
+    Email = "jairo@email.com",
+    Endereco = new {Id = 1, Cidade = "Santos", Pais = "Brasil"}
+};
+Console.WriteLine(aluno2.Endereco.Cidade);
+var alunos = new[]
+{
+        new {Id = 1, Cidade = "Maria", Email = "maria@email.com"},
+        new {Id = 2, Cidade = "Sonia", Email = "sonia@email.com"},
+        new {Id = 3, Cidade = "Bruno", Email = "bruno@email.com"}
+
+};
+Console.WriteLine(alunos.GetType().ToString());
+Console.WriteLine(alunos[0].Email);
