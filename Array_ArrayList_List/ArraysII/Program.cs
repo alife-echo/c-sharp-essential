9af0da9cb58536d361f43473a8df97402b946847
alife-echo <alifesilva90@gmail.com>
2024-05-03 22:41:02 -0300

    feat:arrays III

---
+++ Array_ArrayList_List/ArraysII/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ArraysII");
+
+string[] nomes = { "Ana", "Maria", "Marta", "Paulo", "Carlos" };
+int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Elemento de índice {i} : {numeros[i]}");
+}
+Console.WriteLine("-------------------------------------------");
+for (int i = 0;i < nomes.Length; i++)
+{
+    Console.WriteLine($"Elemento de índice {i} : {nomes[i]}");
+}
---
+++ Array_ArrayList_List/ArraysIII/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Arrays III");
+
+int[] numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+foreach (int numero in numeros )
+{
+    Console.WriteLine(numero);
+}
