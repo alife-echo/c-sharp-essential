6e32da6903c067f7eac4fb14dd45d1ca52199d1a
alife-echo <alifesilva90@gmail.com>
2024-05-20 21:16:26 -0300

    array bidimensional

---
+++ Array_ArrayList_List/ArraysMultiDimensionais/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Arrays MultiDimensionais");
+
+
+int[,] a;
+
+a = new int[2, 2];
+
+
+a[0, 0] = 10;
+a[0, 1] = 11;
+a[1, 0] = 12;
+a[1, 1] = 13;
+
+
+Console.WriteLine(a[0,0]);
+Console.WriteLine(a[0,1]);
+Console.WriteLine(a[1,0]);
+Console.WriteLine(a[1,1]);
+
+
+int[,] b = new int[2, 2] { {20,30}, {40,50} };
+
+Console.WriteLine(b[0, 0]);
+Console.WriteLine(b[0, 1]);
+Console.WriteLine(b[1, 0]);
+Console.WriteLine(b[1, 1]);
