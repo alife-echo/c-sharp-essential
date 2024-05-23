cec05d53eeecb86afb33fd8b1c6beea796c58d79
alife-echo <alifesilva90@gmail.com>
2024-05-22 23:52:37 -0300

    array list I

---
+++ Array_ArrayList_List/ArrayListI/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("ArrayList");
+
+
+
+
+ArrayList list = new ArrayList();
+
+ArrayList list1;
+
+list1 = new ArrayList();
+
+ArrayList lista2 = new();
+
+
+ArrayList list3 = new ArrayList(5);
+
+ArrayList list4;
+
+list4 = new ArrayList(5);
+
+ArrayList lista5 = new(5);
+
+ArrayList lista6 = new();
+
+lista6.Add("Paulo");
+lista6.Add(4.5);
+lista6.Add(5);
+lista6.Add(true);
+lista6.Add("");
+lista6.Add(null);
+
+
+var lista8 = new ArrayList()
+{
+     "Bob"," ",false,null,5,4.5
+};
