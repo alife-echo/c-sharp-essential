dc0034eab5c0653944da7bb8cbffe52c825fc59f
alife-echo <alifesilva90@gmail.com>
2024-05-25 19:35:55 -0300

    remove elementos arraylist

---
+++ Array_ArrayList_List/ArrayListIII/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("ArrayListIII");
+
+
+var lista = new ArrayList() {"Ana",5,true," ",null,1.1,"Zé",3,9,0};
+
+lista.Remove(null);
+lista.RemoveAt(4);
+lista.RemoveRange(0,2);
+
+
+Console.ReadKey();
