6f30b6ba08816ed4eae5ceb35529e71e5b6f8099
alife-echo <alifesilva90@gmail.com>
2024-05-25 19:22:40 -0300

    arralistII

---
+++ Array_ArrayList_List/ArrayListII/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("ArrayList II");
+
+var lista = new ArrayList() {"Maria",5,true,null };
+
+lista.Add(3.5);
+lista.Insert(3, "Paulo");
+
+foreach (var item in lista)
+{
+    Console.WriteLine(item);
+}
+
+
+var lista2 = new ArrayList() { "Bob",6,false,null};
+
+int[] array1 = { 1, 2, 3 };
+
+lista2.AddRange(array1);
+
+lista2.InsertRange(2, array1);
+
+Console.ReadKey();
