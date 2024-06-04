6ba1740c66023e4a4946139e1c21d859d92040cb
alife-echo <alifesilva90@gmail.com>
2024-06-03 23:17:10 -0300

    List II

---
+++ Array_ArrayList_List/ListI/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("List<T>");
+
+
+
+List<string> list;
+list = new List<string>();
+
+List<int> list2 = new List<int>();
+
+List<double> list3 = new();
+
+var list4 = new List<float>();
+
+
+list4.Add(3);
+
+List<string> list5 = new List<string>() 
+{ 
+ "Álife","Bob","Lucas"
+};
---
+++ Array_ArrayList_List/ListTII/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("List<T> II");
+
+
+List<string> list = new List<string>() { "Álife","Cassia","Bob","Richard" };
+
+list.Add("Bob");
+
+list.Insert(0, "Cassia");
+
+List<string> list2 = new List<string>() { "RR", "Gustavo", "Morandi" };
+
+list.AddRange(list2);
+
+List<string> list3 = new List<string>() {"Jax","Fiora","Zac"};
+
+list.InsertRange(0,list3);
+
+Console.WriteLine($"{(list.Contains("Álife") ? "Tem Álife" : "Não tem Álife")}");
+
+Exibir(list);
+
+
+list.Remove("Bob");
+list.RemoveAt(1);
+list.RemoveRange(2,4);
+
+Exibir(list);
+
+list.Sort();
+
+Exibir(list);
+
+list.Clear();
+
+Exibir(list);
+
+
+static void Exibir(List<string> list)
+{
+    foreach (string item in list)
+    {
+        Console.WriteLine(item);
+
+    }
+    Console.WriteLine("\n");
+}
+
