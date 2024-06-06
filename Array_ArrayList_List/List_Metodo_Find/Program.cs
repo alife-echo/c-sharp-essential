ea6345e1f7f1e077279d1229bc928cc4557b4d01
alife-echo <alifesilva90@gmail.com>
2024-06-06 18:48:13 -0300

    metodos findAll

--- Array_ArrayList_List/List_Metodo_Find/Program.cs
+++ Array_ArrayList_List/List_Metodo_Find/Program.cs
@@ -5,12 +5,30 @@ Console.WriteLine("Métodos Find");
 List<string> frutas = new() {"Uva","Banana","Maça","Abacate","Laranja","Morango"};
 
 var fruta1 = frutas.Find(Procura);
-Console.WriteLine($"Predição => {fruta1}");
+Console.WriteLine($"\n(Find) Predição => {fruta1}");
 
 var fruta2 = frutas.Find(f => f.Contains('n'));
-Console.WriteLine($"Lambda => {fruta2}");
-Console.ReadKey();
+Console.WriteLine($"\n(Find) Lambda => {fruta2}");
 
+var fruta3 = frutas.FindLast(f => f.Contains('n'));
+Console.WriteLine($"\n(FindLast) => ${fruta3}");
+
+var fruta4 = frutas.FindIndex(f => f.Contains('n'));
+Console.WriteLine($"\n(FindIndex): indice:{fruta4} item={frutas[fruta4]}");
+
+var fruta5 = frutas.FindLastIndex(f => f.Contains('n'));
+Console.WriteLine($"\n(FindLastIndex): indice:{fruta5} item={frutas[fruta5]}");
+
+var fruta6 = frutas.FindAll(f => f.Contains('n'));
+
+Console.WriteLine("\nFind All : ");
+foreach (var fruta in fruta6)
+{
+    Console.Write($" {fruta}");
+}
+
+
+Console.ReadKey();
 
 static bool Procura(string item)
 {
