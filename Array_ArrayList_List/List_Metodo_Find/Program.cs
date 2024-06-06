8f82a750450261fc51cae6d5607fa1b2a9575f93
alife-echo <alifesilva90@gmail.com>
2024-06-06 14:46:40 -0300

    metodos find

---
+++ Array_ArrayList_List/List_Metodo_Find/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Métodos Find");
+
+
+List<string> frutas = new() {"Uva","Banana","Maça","Abacate","Laranja","Morango"};
+
+var fruta1 = frutas.Find(Procura);
+Console.WriteLine($"Predição => {fruta1}");
+
+var fruta2 = frutas.Find(f => f.Contains('n'));
+Console.WriteLine($"Lambda => {fruta2}");
+Console.ReadKey();
+
+
+static bool Procura(string item)
+{
+    return item.Contains('n');
+}
