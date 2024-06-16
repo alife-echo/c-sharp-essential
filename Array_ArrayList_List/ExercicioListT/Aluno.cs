b6a0b98b813ad39109e5ac73d8b1233227d11605
alife-echo <alifesilva90@gmail.com>
2024-06-15 21:11:52 -0300

    exersice listT

---
+++ Array_ArrayList_List/ExercicioListT/Aluno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioListT;
+public class Aluno
+{
+    private string? nome;
+    private int nota;
+    public string Nome { get => string.IsNullOrEmpty(nome) ? "" : nome; set => nome = value; } 
+    public int Nota { get => nota; set { if (nota < 0) { nota = 0; } else { nota = value; } } }
+    public Aluno() { }
+    public Aluno(string nome,int nota)
+    {
+        this.Nome = nome;
+        this.Nota = nota;
+    }
+   public static void ExbirDadosAluno (List<Aluno> alunos)
+    {
+        Console.WriteLine($"\n----------------- ALUNOS - ORDENADO -----------------");
+        for (int i = 0; i < alunos.Count; i++)
+        {
+            Console.WriteLine($"Nome:{alunos[i].Nome},Nota:{alunos[i].Nota}");
+        }
+        Console.WriteLine($"\n-----------------------------------------------------");
+    }
+    public static void MenuAluno()
+    {
+        Console.WriteLine($"----------------- SIGAA 2 -----------------");
+        Console.WriteLine($"Sececione As Opções Para Alunos");
+        Console.WriteLine($"1 - Exibir Alunos");
+        Console.WriteLine($"2 - Adicionar Aluno");
+        Console.WriteLine($"3 - Excluir Aluno");
+        Console.WriteLine($"4 - Localizar Aluno");
+        Console.WriteLine($"5 - Exibir Alunos com nota maior que a nota de corte");
+        Console.WriteLine($"6 - Sair Sistema");
+        Console.WriteLine($"-------------------------------------------");
+
+    }
+    public static void FiltroNotas (List<Aluno> alunos,int notaCorte)
+    {
+        var filtro = alunos.Where(a => a.Nota > notaCorte).ToList();
+        Console.WriteLine($"Aluno com nota maior que {notaCorte}");
+        foreach (Aluno a in filtro)
+        {
+            Console.WriteLine($"Nome:{a.Nome},Nota:{a.Nota}");
+        }
+    }
+}
---
+++ Array_ArrayList_List/ExercicioListT/Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExercicioListT;
+Console.WriteLine("List<T> - Exercício");
+
+
+var alunos = new List<Aluno>();
+Console.WriteLine("\n");
+Aluno.MenuAluno();
+int _ = 0;
+for(;;)
+{
+    int opcao = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("\n");
+    if (opcao == 1)
+    {
+       
+        Aluno.ExbirDadosAluno(alunos);
+        Aluno.MenuAluno();
+    }
+    else if (opcao == 2)
+    {
+
+            Console.WriteLine($"-------------------------------------------");
+            Console.WriteLine("Informe o nome do aluno:");
+            string nome = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Informe a nota do aluno:");
+            int nota = Convert.ToInt32(Console.ReadLine());
+            alunos.Add(new Aluno(nome, nota));
+            Console.Clear();
+            Aluno.MenuAluno();
+
+    }
+   else if (opcao == 3)
+    {
+
+            Aluno.ExbirDadosAluno(alunos);
+            Console.WriteLine("Digite o Nome do aluno");
+            string nomeAlunoExcluir = Console.ReadLine() ?? string.Empty;
+            alunos.RemoveAt(alunos.FindIndex(a => a.Nome.Contains(nomeAlunoExcluir)));
+            Console.Clear();
+            Aluno.MenuAluno();
+
+
+    }
+    else if (opcao == 4)
+    {
+
+            Aluno.ExbirDadosAluno(alunos);
+            Console.WriteLine("Digite o nome do aluno para localizar ou 6 para voltar");
+            string nomeAlunoLocalizar = Console.ReadLine() ?? string.Empty;
+            var aluno = alunos.Find(a => a.Nome.Contains(nomeAlunoLocalizar));
+            Console.WriteLine($"Nome:{aluno?.Nome},Nota:{aluno?.Nota}");
+            if(Convert.ToInt32(Console.ReadLine()) == 6 || Convert.ToInt32(nomeAlunoLocalizar) == 6)
+            {
+            Console.Clear();
+            Aluno.MenuAluno();
+            }
+           
+        
+    }
+   else if (opcao == 5)
+    {
+            Console.WriteLine("Digite a nota de corte ou -1 para sair");
+            int notaCorte = Convert.ToInt32(Console.ReadLine());
+            Aluno.FiltroNotas(alunos, notaCorte);
+            if (Convert.ToInt32(Console.ReadLine()) == -1 || notaCorte == -1 || notaCorte < 0)
+            {
+                Console.Clear();
+                Aluno.MenuAluno();
+            }
+        
+
+    }
+    else if (opcao == 6)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ocorreu um erro,contate o administrador");
+    }
+    _++;
+}
