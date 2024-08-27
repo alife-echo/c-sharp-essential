d35da3a8407befa2396bdddb9240a46a72e579fb
alife-echo <alifesilva90@gmail.com>
2024-08-27 00:30:59 -0300

    add study inheritance and behaviors

---
+++ programacaoOrientadaObjetos/HerancaConstrutores/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Aluno aluno1 = new Aluno();
+Aluno aluno2 = new Aluno("Jose");
+
+Console.ReadKey();
+
+
+class Pessoa
+{
+    public Pessoa()
+    {
+        Console.WriteLine("Construtor da classe Pessoa");
+    }
+    public Pessoa(string nome)
+    {
+        Console.WriteLine("Constrotor da classe pessoa Com Parametro");
+    }
+}
+
+//derivada
+class Aluno : Pessoa
+{
+     public Aluno() : base()
+    {
+        Console.WriteLine("Construtor da classe Aluno");
+    }
+    public Aluno(string nome) : base(nome)  
+    {
+        Console.WriteLine("Construtor da classe Aluno com parametro");
+    }
+}
---
+++ programacaoOrientadaObjetos/herancasEComportamento/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+ClasseDerivada cd = new();
+cd.VerificaAcesso();
+
+Console.ReadKey();
+
+
+
+public class ClasseDerivada : ClasseBase
+{
+
+    public void VerificaAcesso()
+    {
+        Public_Membro();
+        Protected_Membro();
+        Internal_Membro();
+        Protected_Internal_Membro();
+        //Private_Membro();
+        Console.WriteLine("\n Acesso ao campo private : private_var usando uma proprieade");
+                                                                                        //Propriedade
+        int soma = public_var + protected_var + internal_var + protected_internal_var + Private_Var;
+        Console.WriteLine($"\n Total Soma:{soma}");
+    }
+}
+
+public class ClasseBase
+{
+    //CAMPOS
+    public int public_var = 1;
+    internal int internal_var = 2;
+    protected int protected_var = 3;
+    protected internal int protected_internal_var = 4;
+    private int private_var = 5;
+
+    // Só posso acessar o campo private_var criando uma propriedade get e set para private_var
+    //Propriedades
+    public int Private_Var
+    {
+        get { return private_var; }
+        set { private_var = value; }
+    }
+    public void Public_Membro()
+    {
+        Console.WriteLine("ClasseBase - Método Public");
+    }
+    protected void Protected_Membro()
+    {
+        Console.WriteLine("ClasseBase - Método Protected");
+    }
+    internal void Internal_Membro()
+    {
+        Console.WriteLine("ClasseBase - Internal");
+    }
+    protected internal void Protected_Internal_Membro()
+    {
+        Console.WriteLine("ClasseBase - Método Protected Internal");
+    }
+    private void Private_Membro()
+    {
+        Console.WriteLine("ClasseBase - Método private");
+    }
+}
