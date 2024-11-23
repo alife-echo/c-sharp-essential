3eb643b97ab438f0c011f019e53d1cefeacb192e
alife-echo <alifesilva90@gmail.com>
2024-11-23 17:07:21 -0300

    "Generics III"

---
+++ programacaoOrientadaObjetos/ExersciseGenerics/ExersciseGenerics/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Collections;
+
+Generic<ArrayList> objArray1 = new Generic<ArrayList>();
+objArray1.Add(new ArrayList(){2,"Steve","",true});
+
+foreach (var data in objArray1[0])
+{
+    Console.WriteLine(data);
+}
+
+Generic<Student> objStudent1 = new Generic<Student>();
+objStudent1.Add(new Student(){Name = "Alife"});
+
+Console.WriteLine(objStudent1[0].Name);
+
+
+
+Generic<int> obj1 = new Generic<int>();
+obj1.Add(10);
+Console.WriteLine(obj1[0]);
+
+
+
+
+
+public class Generic<T>
+{
+    
+    int count = 0;
+    T[] array_generic = new T[5];
+
+    public void Add(T currentObject)
+    {
+     
+        if (count < 5)
+        {
+            array_generic[count] = currentObject;
+        }
+
+        count++;
+    } 
+    public T this[int index]
+    {
+        get => array_generic[index];
+        set => array_generic[index] = value;
+    }
+}
+
+public class Student
+{
+    public string? Name { get; set; }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ programacaoOrientadaObjetos/GenericsIII/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Collections;
+
+ClassGeneric<Student> obj1 = new ClassGeneric<Student>();
+obj1.Add(new Student{Name = "Alife"});
+
+Console.WriteLine(obj1[0].Name);
+
+
+public class  ClassGeneric<T>
+{ 
+     ArrayList list = new ArrayList(5);
+     int count = 0;
+
+    public void Add(T item)
+    {
+        if (count < 5)
+        {
+            objects[count] = item;
+        }
+
+        count++;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            return objects[index];
+        }
+        set
+        {
+            objects[index] = value;
+        }
+    }
+}
+
+public class Student
+{
+    public string? Name { get; set; }
+}
