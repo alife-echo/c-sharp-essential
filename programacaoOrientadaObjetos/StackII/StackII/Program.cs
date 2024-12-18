1cbe416dd214bc32a5bbe2afc4e744ceca1c2b2c
alife-echo <alifesilva90@gmail.com>
2024-12-18 09:32:46 -0300

    StackII

---
+++ programacaoOrientadaObjetos/StackII/StackII/Program.cs
@@ -0,0 +1,18 @@
+﻿using StackII;
+    
+Console.WriteLine("\n ### Stack - Undo ###");
+
+EditorTexto editor = new EditorTexto();
+Console.WriteLine("\n Digitando... \n");
+editor.DigitarChar('a');
+editor.DigitarChar('b');
+editor.DigitarChar('c');
+
+Console.WriteLine("\n Fazendo o Undo ... \n");
+editor.Undo();
+
+Console.WriteLine("\n Redigitando ....");
+editor.DigitarChar('c');
+editor.DigitarChar('d');
+editor.DigitarChar('e');
+Console.ReadKey();
