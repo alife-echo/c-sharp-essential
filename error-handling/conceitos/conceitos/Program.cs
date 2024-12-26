9884ba7537f1886a007e44bd1da299737ceeab7e
alife-echo <alifesilva90@gmail.com>
2024-12-26 16:16:46 -0300

    try-catch-finally

---
+++ error-handling/conceitos/conceitos/Program.cs
@@ -0,0 +1,49 @@
+﻿
+// 1 - Maneira
+try
+{
+
+}
+catch
+{
+    
+}
+// 2 - Maneira
+try
+{
+
+}
+catch
+{
+
+}
+finally
+{
+    
+}
+// 3 - Maneira
+try
+{
+
+}
+catch
+{
+
+}
+catch
+{
+
+}
+finally
+{
+    
+}
+// 1 - Maneira
+try
+{
+
+}
+finally
+{
+    
+}
