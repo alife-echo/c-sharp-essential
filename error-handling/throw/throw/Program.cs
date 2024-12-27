ab432006cdafcd53190e56d843e1a659f1c110d8
alife-echo <alifesilva90@gmail.com>
2024-12-26 21:30:40 -0300

    launch-exceptions

---
+++ error-handling/throw/throw/Program.cs
@@ -0,0 +1,57 @@
+﻿
+try
+{
+    A.ProcessarA();
+}
+catch(Exception e)
+{
+    Console.WriteLine("\nTratando erro na main");
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+}
+
+
+Console.ReadKey();
+
+public class A
+{
+    
+    public static void ProcessarA()
+    {
+        try
+        {
+            B.ProcessarB();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+            Console.WriteLine("Tratando o erro em A");
+        }
+
+    }
+}
+
+public class B
+{
+    public static void ProcessarB()
+    {
+        try
+        {
+            C.ProcessarC();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+            Console.WriteLine("Tratando erro em B");
+        }
+       
+    }
+}
+
+public class C
+{
+    public static void ProcessarC()
+    {
+        throw new NotImplementedException("Método não implementado");
+    }
+}
