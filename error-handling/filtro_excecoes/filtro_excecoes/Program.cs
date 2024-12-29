5a4f35b2726657a20a54b16357d3d8bb746bfa48
alife-echo <alifesilva90@gmail.com>
2024-12-28 22:14:01 -0300

    filter_exceptions

---
+++ error-handling/filtro_excecoes/filtro_excecoes/Program.cs
@@ -0,0 +1,30 @@
+﻿int dividendo = 0;
+int divisor = 0;
+
+try
+{
+    Console.WriteLine("Digite o dividendo");
+    dividendo = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite o divisor");
+    divisor = Convert.ToInt32(Console.ReadLine());
+    int resultado = dividendo / divisor;
+    Console.WriteLine($"\n{dividendo} / {divisor} = {resultado}");
+}
+/*
+catch (DivideByZeroException) when (DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+{
+    Console.WriteLine("Hoje é sabado, não trabalhamos mongol");
+}*/
+catch (Exception) when ((dividendo / divisor) == 0)
+{
+    Console.WriteLine("Esta linha nunca sera lida");
+}
+catch (Exception ex) when (ex.Message.Contains("format"))
+{
+    Console.WriteLine("Filtro de exceção com format");
+    Console.WriteLine(ex.StackTrace);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
