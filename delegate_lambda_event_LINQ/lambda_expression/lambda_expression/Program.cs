01a528754cd4bdd2c0cdd23c4bbbf1ca74a39c39
alife-echo <alifesilva90@gmail.com>
2025-01-06 20:57:38 -0300

    lambda expression

---
+++ delegate_lambda_event_LINQ/lambda_expression/lambda_expression/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+List<string> nomes = new List<string>();
+
+
+nomes.Add("John");
+nomes.Add("Doe");
+nomes.Add("Mary");
+nomes.Add("Paulo");
+nomes.Add("Fred");
+nomes.Add("James");
+
+
+// tranformado em lambda
+string? resultado = nomes.Find(nome => nome.Equals("Paulo"));
+
+Console.WriteLine(resultado);
+
+Console.ReadKey();
