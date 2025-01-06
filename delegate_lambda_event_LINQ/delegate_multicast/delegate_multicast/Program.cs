744a356d721562ae12f277bb9a9e49b85e2fb9db
alife-echo <alifesilva90@gmail.com>
2025-01-06 17:19:52 -0300

    multicast delegate

---
+++ delegate_lambda_event_LINQ/delegate_multicast/delegate_multicast/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+NotificationDelegate notifyUsers = EmailNotification;
+notifyUsers += SmsNotification;
+notifyUsers += PushNotification;
+
+Console.WriteLine("Enviando notificações");
+notifyUsers("Novo evento disponivel");
+
+notifyUsers -= SmsNotification;
+Console.WriteLine("Enviando apenas para dispositivos e Emails");
+notifyUsers("Atualização do evento!");
+
+Console.ReadKey();
+
+static void SmsNotification(string message)
+{
+    Console.WriteLine($"[SMS] {message}");
+}
+
+static void EmailNotification(string message)
+{
+    Console.WriteLine($"[EMAIL] {message}");
+}
+
+static void PushNotification(string message)
+{
+    Console.WriteLine($"[PUSH] {message}");
+}
+
+public delegate void NotificationDelegate( string message );
+
+
