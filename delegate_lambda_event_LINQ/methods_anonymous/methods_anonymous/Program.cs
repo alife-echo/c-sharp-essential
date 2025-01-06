2a6b3f449eb223dba3cb4f91d45dc163b05d1555
alife-echo <alifesilva90@gmail.com>
2025-01-06 19:21:35 -0300

    methods anonymous

---
+++ delegate_lambda_event_LINQ/methods_anonymous/methods_anonymous/Program.cs
@@ -0,0 +1,16 @@
+﻿List<string> nomes = new List<string>();
+
+
+nomes.Add("John");
+nomes.Add("Doe");
+nomes.Add("Mary");
+nomes.Add("Paulo");
+nomes.Add("Fred");
+nomes.Add("James");
+
+
+string? resultado = nomes.Find(delegate (string nome) { return nome.Equals("Paulo"); });
+
+Console.WriteLine(resultado);
+
+Console.ReadKey();
