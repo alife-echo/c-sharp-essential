2ae873c1b3cdfd779c071ae585a9d0060e48d9c2
alife-echo <alifesilva90@gmail.com>
2025-01-07 19:29:42 -0300

    func

---
+++ delegate_lambda_event_LINQ/Func/Func/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Digite um numero");
+
+var numero = Convert.ToDouble(Console.ReadLine());
+
+Func<double,double> raizQuadrada = x => Math.Sqrt(x);
+
+double resultado = raizQuadrada(numero);
+
+Console.WriteLine($"A raiz quadrada do numero : {numero} é : {resultado}");
+
+Console.ReadKey();
+
