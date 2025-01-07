b6ca692b72c1a024f693fa5a11b006da8118dcc0
alife-echo <alifesilva90@gmail.com>
2025-01-07 17:05:25 -0300

    delegate predicate

---
+++ delegate_lambda_event_LINQ/delegate_predicate/delegate_predicate/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.WriteLine("Digite um número : ");
+var numero = Convert.ToInt32(Console.ReadLine());
+
+Predicate<int> delegatePar = (n) => n%2 == 0;
+
+var res = delegatePar(numero) ? $"O numero {numero} é PAR" : $"O numero é {numero} é IMPAR";
+
+Console.WriteLine(res);
