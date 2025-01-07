ed230a125db9285ede20cf0ee44abf57fa287758
alife-echo <alifesilva90@gmail.com>
2025-01-07 18:16:19 -0300

    delegate action

---
+++ delegate_lambda_event_LINQ/delegateAction/delegateAction/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Informe um número inteiro.");
+var numero = Convert.ToInt32(Console.ReadLine());
+
+
+Action<int> res = (x) => Console.WriteLine(x * 2);
+
+
+res(numero);
+
+Console.ReadKey();
+
