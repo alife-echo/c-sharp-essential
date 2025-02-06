09ecfd622f6075648e71a50fb263f0a7a0d72905
alife-echo <alifesilva90@gmail.com>
2025-02-06 00:24:45 -0300

    stream

---
+++ file_directory_and_streams/streams/streams/Program.cs
@@ -0,0 +1,35 @@
+﻿
+var caminhoArquivo = @"/home/alife-echo/Desktop/teste_files/txt/poesia.txt";
+
+FileStream fs = null;
+StreamReader read = null;
+
+
+try
+{
+    fs = new FileStream(caminhoArquivo, FileMode.Open,
+        FileAccess.Read); // cria a vinculação do do caminho do arquivo com o filestream configurando os metodos de acesso e leitura
+    read = new StreamReader(fs); // usa o StreamReader para converter os bytes em strings para serem lidas
+
+    string? row;
+
+    while ((row = read.ReadLine()) != null) // enquando houver linhas
+    {
+        Console.WriteLine(row); //printe as linhas
+    }
+}
+catch (IOException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally // fecha os recursos alocadas, 
+{
+    if (read != null) read.Close();
+    if(fs != null) fs.Close();
+
+}
+Console.ReadKey();
