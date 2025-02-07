7619ec2116ad02b16138e56d94fcb2e37ad244a5
alife-echo <alifesilva90@gmail.com>
2025-02-06 22:47:48 -0300

    streams with using

---
+++ file_directory_and_streams/streams_using/streams_using/Program.cs
@@ -0,0 +1,24 @@
+﻿var caminhoArquivo = @"/home/alife-echo/Desktop/teste_files/txt/poesia.txt";
+
+try
+{
+    /*  1 maneira de ler arquivos com using
+    using FileStream fs = new FileStream(caminhoArquivo, FileMode.Open, FileAccess.Read);
+    using StreamReader read = new StreamReader(fs);
+    */
+    using StreamReader read = File.OpenText(caminhoArquivo); // 2 maneira  de ler com using
+    string? row;
+
+    while ((row = read.ReadLine()) != null)
+    {
+        Console.WriteLine(row);
+    }
+}
+catch (IOException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
