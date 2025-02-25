29a3ffc1fbf251ac5cc103a111c51cb502924024
alife-echo <alifesilva90@gmail.com>
2025-02-24 22:36:56 -0300

    exercise serializer json and xml

---
+++ serialization_and_deserialization/exercicio_serialization/exercicio_serialization/Aluno.cs
@@ -0,0 +1,22 @@
+﻿
+namespace exercicio_serialization;
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public int Idade { get; set; }
+
+        public Aluno() { }
+        public Aluno(int id, string nome, string email, int idade)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Idade = idade;
+        }
+    
+}
+
---
+++ serialization_and_deserialization/exercicio_serialization/exercicio_serialization/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+using exercicio_serialization;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+var listAlunos = new List<Aluno>()
+{
+    new (1,"Alife","alifesilva90@gmail.com", 20),
+    new (2,"Bob","bob30@gmail.com",30),
+    new (3,"Richard","richad@9gmail.com",25)
+};
+
+var pathJson = @"C:\Users\alife\OneDrive\Desktop\dados\serializacao_json\Alunos.json";
+
+
+using (FileStream fs = new FileStream(pathJson, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+{
+    JsonSerializer.Serialize(fs, listAlunos);
+}
+Console.WriteLine("Serialização de alunos realizado com sucesso - JSON");
+ 
+
+
+var readFileAluno = File.ReadAllText(pathJson);
+List<Aluno>? alunosDesserializados = JsonSerializer.Deserialize<List<Aluno>>(readFileAluno);
+
+Console.WriteLine("\n");
+Console.WriteLine("Desserialização de alunos realizada com sucesso - JSON");
+Console.WriteLine("\n");
+
+foreach (Aluno aluno in alunosDesserializados)
+{
+    Console.WriteLine($"Id:{aluno.Id},Nome:{aluno.Nome},Email:{aluno.Email},Idade:{aluno.Idade}");
+}
+
+
+
+var pathXml = @"C:\Users\alife\OneDrive\Desktop\dados\serialization_xml\Alunos.xml";
+
+XmlSerializer serializer = new XmlSerializer(typeof(List<Aluno>));
+
+using(StreamWriter sw = new StreamWriter(pathXml))
+{
+    serializer.Serialize(sw, listAlunos);
+}
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Serialização de alunos realizado com sucesso - XML");
+
+Console.WriteLine("\n");
+using (StreamReader sr = new StreamReader(pathXml))
+{
+    var alunoDesserializadoXml = (List<Aluno>?)serializer.Deserialize(sr);
+
+    foreach (Aluno aluno in alunoDesserializadoXml)
+    {
+        Console.WriteLine($"Id:{aluno.Id},Nome:{aluno.Nome},Email:{aluno.Email},Idade:{aluno.Idade}");
+    }
+}
