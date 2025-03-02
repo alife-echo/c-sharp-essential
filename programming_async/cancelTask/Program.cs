f49234c1d19376890b12ad20f51ae81d6a95f758
alife-echo <alifesilva90@gmail.com>
2025-03-01 21:38:46 -0300

    cancel task

---
+++ programming_async/cancelTask/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Diagnostics;
+
+
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+try
+{
+    await CancelTaskWithTimeOut(2000);
+}
+catch (TaskCanceledException)
+{
+    Console.WriteLine($"Tarefa cancelada: tempo expirado após {stopwatch.Elapsed}\n");
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine($"Operação cancelada: tempo expirado após {stopwatch.Elapsed}\n");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro inesperado: {ex.Message}");
+}
+static async Task CancelTaskWithTimeOut(int time)
+{
+    using (var cts_time = new CancellationTokenSource(time))
+    {
+     
+        try
+        {
+
+            var resultado = await OperacaoDeLongaDuracao(10, cts_time.Token);
+
+            Console.WriteLine($"resultado:{resultado}");
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+}
+static  Task<int> OperacaoDeLongaDuracao(int valor, CancellationToken cancellationToken = default) // default significa que o argumento é opcional
+{
+    return Task.Run(() =>
+    {
+
+            int resultado = 0;
+
+            for (int i = 0; i < valor; i++)
+            {
+
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                } 
+            Console.WriteLine($"Processo : {i}/{valor}");
+            Thread.Sleep(1000);
+                
+                resultado += i;
+            }
+            return resultado;
+              
+   
+    }, cancellationToken);
+}
---
+++ programming_async/valuetask_and_valuetask_t/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Informe o primeiro numero inteiro");
+var num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o segundo numero inteiro");
+var num2 = Convert.ToInt32(Console.ReadLine());
+
+var soma =  CalcularSomaAsync(num1, num2).Result;
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"\n {num1} + {num2} = {num1 + num2}");
+
+Console.ReadKey();
+static async ValueTask<int> CalcularSomaAsync(int num1, int num2)
+{
+    if (num1 == 0 && num2 == 0)
+        return 0;
+
+    return await Task.Run(() => num1 + num2);
+}
