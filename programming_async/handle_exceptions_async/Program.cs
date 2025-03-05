6a09d3795f6a293bb37fa0c9502267770d26914b
alife-echo <alifesilva90@gmail.com>
2025-03-05 12:18:35 -0300

    how to handler exception on methods sync and async

---
+++ programming_async/handle_exceptions_async/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+TesteAsync t = new();
+
+// apenas chame o método
+t.ChamaTarefaAsync();
+
+
+
+Console.ReadKey();
+class TesteAsync
+{
+    public Task MinhaTarefaAsync()
+    {
+       return Task.Run(() =>
+        {
+            Task.Delay(2000);
+            throw new Exception("Minha Exception");
+        });
+
+    }
+    public async void ChamaTarefaAsync()
+    {
+        // mova o tratamento para o codigo assíncrono
+        try
+        {
+            await MinhaTarefaAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Bloco catch foi executado");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}*/
+
+await LancarExceptionAsync();
+
+Console.ReadKey();
+
+static async Task LancarExceptionAsync()
+{
+    try
+    {
+        var primeiraTask = Task.Run(() =>
+        {
+            Task.Delay(1000);
+            throw new IndexOutOfRangeException("IndexOutOfRangeException lançada explicitamente");
+
+        });
+        await primeiraTask;
+    }catch(Exception ex)
+    {
+        Console.WriteLine("Bloco catch executado");
+        Console.WriteLine(ex.Message);
+    }
+}
