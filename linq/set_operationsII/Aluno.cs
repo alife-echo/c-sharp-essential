a5f9164d814de942332fd44fcf9b2c370a8ceb75
alife-echo <alifesilva90@gmail.com>
2025-03-21 20:43:53 -0300

    sets operation II

---
+++ linq/set_operationsII/Aluno.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace set_operationsII;
+public class Aluno
+{
+    public string Nome { get; set; } = string.Empty;
+    public int Idade { get; set; } 
+    public DateTime Nascimento { get; set; }
+}
+
---
+++ linq/set_operationsII/Program.cs
@@ -0,0 +1,50 @@
+﻿using set_operationsII;
+
+var fonte1 = new List<int>() { 1, 2, 3, 4, 5, 6 };
+var fonte2 = new List<int>() {1,3,5,8,9,10};
+
+var resultado = fonte1.Intersect(fonte2).ToList();
+Exibir(resultado);
+
+string[] font1Country = { "Brasil", "USA", "UK", "Argentina", "China" };
+string[] font2Country = { "Brasil", "uk", "Argentina", "França", "Japão" };
+var resultadoCidades = font1Country.Intersect(font2Country,StringComparer.OrdinalIgnoreCase).ToList();
+Exibir(resultadoCidades);
+
+var fontTurmaA = FonteDeDados.GetTurmaA();
+var fontTurmaB = FonteDeDados.GetTurmaB();
+
+IEnumerable<Aluno> alunosComMesmoAnoNascimento = fontTurmaA.IntersectBy(fontTurmaB.Select(b => b.Nascimento.Year), a => a.Nascimento.Year).ToList();
+Console.WriteLine("Aluno com mesmo ano de nascimento");
+
+foreach (var alunoMesmaData in alunosComMesmoAnoNascimento)
+{
+    Console.WriteLine($"{alunoMesmaData.Nome} : {alunoMesmaData.Idade} : {alunoMesmaData.Nascimento}");
+}
+
+
+// uso normal da operação Union
+var resultadoUnion = fonte1.Union(fonte2);
+Exibir(resultadoUnion);
+
+// union com equality comparer para ignorar o case sensitive
+var unionSemCaseSensitive = font1Country.Union(font2Country, StringComparer.OrdinalIgnoreCase);
+Exibir(unionSemCaseSensitive);
+
+// no primeiro argumento eu apenas passo a segunda fonte de dados e no terceiro argumento
+var todosAlunosSemRepeticao = fontTurmaA.UnionBy(fontTurmaB, p => p.Nome);
+
+foreach (var alunoNomeUnico in todosAlunosSemRepeticao)
+{
+    Console.WriteLine($"{alunoNomeUnico.Nome} : {alunoNomeUnico.Idade} : {alunoNomeUnico.Nascimento}");
+}
+
+static void Exibir<T>(IEnumerable<T> collection)
+{
+    foreach (var value in collection)
+    {
+        Console.Write($"{value} ");
+    }
+    Console.WriteLine();
+}
+
