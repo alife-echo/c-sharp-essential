7cd076733aad89ff9555b90c37a32c31cf502479
alife-echo <alifesilva90@gmail.com>
2025-04-19 12:17:54 -0300

    backup

---
+++ atualizacoes_c#/init_only_properties/Cliente.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace init_only_properties;
+class Cliente
+{
+    private readonly string? _nome;
+    private readonly string? _email;
+    public string Nome
+    {
+        get => _nome ;
+        init => _nome = string.IsNullOrWhiteSpace(value) ?
+            throw new ArgumentException("Não pode ser null nem vazio", nameof(Nome))
+            : value;
+    }
+    public string Email 
+    {
+        get => _email;
+        init => _email = string.IsNullOrWhiteSpace(value) ?
+            throw new ArgumentException("Não pode ser null nem vazio", nameof(Email))
+            : value;
+    }
+
+}
+
---
+++ atualizacoes_c#/init_only_properties/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using init_only_properties;
+
+var cliente = new Cliente
+{
+    Nome = "Álife",
+    Email = "bobsilva@gmail.com"
+};
+
+cliente.Nome = "regis";
+
+
+
+/*
+var cliente = new Cliente("Álife", "bobsilva@gmail.com");
+
+cliente.Nome = "regis";
+*/
+
+
+Console.ReadLine();
