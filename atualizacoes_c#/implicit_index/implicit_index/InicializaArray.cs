913bb0c7c9781e6dd39f579ae4ab597ddb068e26
alife-echo <alifesilva90@gmail.com>
2025-05-05 16:39:17 -0300

    implicit_index

---
+++ atualizacoes_c#/implicit_index/implicit_index/InicializaArray.cs
@@ -0,0 +1,6 @@
+namespace implicit_index;
+
+public class InicializaArray
+{
+    public int[] Inteiros { get; set; } = new int[5];
+}
---
+++ atualizacoes_c#/implicit_index/implicit_index/Program.cs
@@ -0,0 +1,28 @@
+﻿using implicit_index;
+
+var inicializaLista = Enumerable.Range(1, 10);
+
+var outraLista = new List<int>(inicializaLista)
+{
+  [^1] = 15
+};
+
+Console.WriteLine(string.Join(", ", outraLista));
+
+// posso modificar um array na sua inicialização utilizando tambem indice implicito
+InicializaArray vetor = new()
+{
+  Inteiros =
+  {
+    [0] = 500,
+    [^4] = 5,
+    [^3] = 300,
+    [^2] = 200,
+    [^1] = 100,
+  }
+};
+
+Console.WriteLine(vetor.Inteiros[^1]);
+Console.WriteLine(vetor.Inteiros[^2]);
+Console.WriteLine(vetor.Inteiros[^3]);
+Console.WriteLine(string.Join(", ", vetor.Inteiros));
