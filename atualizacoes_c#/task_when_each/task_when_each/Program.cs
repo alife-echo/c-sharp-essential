9bdfbacba3a121c6cb6df928e96771268b7d388c
alife-echo <alifesilva90@gmail.com>
2025-05-05 19:46:36 -0300

    TaskWhenEach

---
+++ atualizacoes_c#/task_when_each/task_when_each/Program.cs
@@ -0,0 +1,24 @@
+﻿List<Task<int>> listaTarefas  =
+[
+    SimulaOperacaoAsync(2000),
+    SimulaOperacaoAsync(5000),
+    SimulaOperacaoAsync(3000),
+    SimulaOperacaoAsync(1000)
+];
+var s = Task.WhenEach(listaTarefas);
+// aguarda todas as tarefas serem concluidas para exibir o resultado
+//var tarefas = await Task.WhenAll(listaTarefas);
+
+// devo colocar um await pois ao iterar cada tarefa eu tenho um Task<int> e para capturar seu valor eu preciso do await
+await foreach (var tarefa in Task.WhenEach(listaTarefas))
+{
+    Console.WriteLine(await tarefa); // iterar o inice eu tenho um Task<int> então eu preciso de um await para capturar e exibir seu resultado
+}
+
+
+
+static async Task<int> SimulaOperacaoAsync(int delay)
+{
+    await Task.Delay(delay); // simula um trabalho assíncrono
+    return delay;
+}
